298dc4ba932bdf75376c3f8bc9605286ab576592
J.D. DeVaughn-Brown <jd@chadwickschool.org>
2018-07-02 07:46:35 -0400

    Populate first name field with search input variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
   namePattern = new RegExp("^([A-Za-z'-]+)$");
   phoneNumberPattern = new RegExp("([0-9\(\)-]+$)");
   public phoneMask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 
+  public searchInput = 'Test Search';
+
+  ngOnInit(): void {
+    document.getElementById("firstName").setAttribute("value", this.searchInput);
+  }
 
   public couldBeName(token: string) {
     return this.namePattern.test(token);
